3df4e5ff20b2cf20b25c08e117306f4fbd56e555
clefalto <hamomelett@gmail.com>
2025-05-30 12:20:55 -0700

    dialogue

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,6 +9,16 @@ public class NPC : MonoBehaviour
     public GameObject spawnPoint;
     public GameObject moveTarget;
 
+    public Canvas textCanvas;
+    public GameObject textBox; // box for displaying mr. text, should be a child of this go
+
+    // dialogues
+    public TextAsset dialogue; // all the dialogue is just in a text file
+    private List<string> dialogueLines;
+    private int currentLine = 0;
+    public float dialogueShowTime = 7f; // how long each line of dialogue is shown, seven seconds is probably enough :3
+
+    // movement
     public float moveSpeed = 5.0f;
     public float bobSpeed = 5.0f;
 
@@ -42,5 +54,37 @@ public class NPC : MonoBehaviour
 
             yield return null;
         }
+
+        OnReachedSpeakPos();
+    }
+
+    void OnReachedSpeakPos()
+    {
+        Debug.Log("Reached speak position!");
+
+        // https://stackoverflow.com/questions/67744910/importing-each-line-from-text-file-from-resources-in-unity-to-list-in-c-sharp
+        var dialogueText = dialogue.text;
+        var allLines = dialogueText.Split("\n");
+        dialogueLines = new List<string>(allLines);
+
+        if (dialogueLines.Count > 0)
+        {
+            StartCoroutine(DialogueLoop()); 
+        }
+    }
+
+    IEnumerator DialogueLoop()
+    {
+        textCanvas.gameObject.SetActive(true);
+
+        while (currentLine < dialogueLines.Count)
+        {
+            Debug.Log("dialogue lines: " + dialogueLines[currentLine].ToString());
+            textBox.GetComponent<TextMeshProUGUI>().text = dialogueLines[currentLine];
+            currentLine++;
+            yield return new WaitForSeconds(dialogueShowTime);
+        }
+
+        textCanvas.gameObject.SetActive(false);
     }
 }
