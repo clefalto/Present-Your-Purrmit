c479d922fb5be4c4842adfbfc9da9e4a4c2ca31e
clefalto <hamomelett@gmail.com>
2025-06-01 17:16:34 -0700

    dialogue works with accept/rejecting

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,15 +7,20 @@ using UnityEngine.UIElements;
 
 public class NPC : MonoBehaviour
 {
-    public GameObject spawnPoint;
-    public GameObject moveTarget;
+    public GameObject spawnLocation;
+    public GameObject dialogueLocation;
+    public GameObject endLocation;
+
+    private GameObject target;
 
     public Canvas textCanvas;
     public GameObject textBox; // box for displaying mr. text, should be a child of this go
 
     // dialogues
-    public TextAsset dialogue; // all the dialogue is just in a text file
-    private List<string> dialogueLines;
+    bool spoken = false;
+    public TextAsset initialDialogue; // plays when the npc reaches their LOCATION
+    public TextAsset acceptDialogue; // plays when the npc is accepted
+    public TextAsset rejectDialogue; // plays when the npc is rejected
     private int currentLine = 0;
     public float dialogueShowTime = 7f; // how long each line of dialogue is shown, seven seconds is probably enough :3
 
@@ -22,20 +28,22 @@ public class NPC : MonoBehaviour
     public float moveSpeed = 5.0f;
     public float bobSpeed = 5.0f;
 
+    public delegate void DialogueCallback();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = spawnPoint.transform.position;
-        StartCoroutine(MoveTowardsTarget());
+        transform.position = spawnLocation.transform.position;
+        StartCoroutine(MoveTowardsTarget(dialogueLocation));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position += new Vector3(0.0f, Mathf.Sin(Time.time * bobSpeed) * Time.deltaTime, 0.0f); // uncomment if you want constant bobbin
     }
 
-    IEnumerator MoveTowardsTarget()
+    IEnumerator MoveTowardsTarget(GameObject moveTarget)
     {
         Vector3 horizPos = new Vector3(transform.position.x, moveTarget.transform.position.y, transform.position.z);
         float planarDist = (new Vector3(transform.position.x, 0.0f, transform.position.z) - new Vector3(moveTarget.transform.position.x, 0.0f, moveTarget.transform.position.z)).magnitude;
@@ -55,27 +63,37 @@ public class NPC : MonoBehaviour
             yield return null;
         }
 
-        OnReachedSpeakPos();
+        if (!spoken)
+        {
+            OnReachedSpeakPos();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnReachedSpeakPos()
     {
         Debug.Log("Reached speak position!");
 
+        StartCoroutine(DialogueLoop(initialDialogue, WaitForVerdict));
+        spoken = true; // we have now spoken
+    }
+
+    List<string> ParseDialogue(TextAsset dialogue)
+    {
         // https://stackoverflow.com/questions/67744910/importing-each-line-from-text-file-from-resources-in-unity-to-list-in-c-sharp
         var dialogueText = dialogue.text;
         var allLines = dialogueText.Split("\n");
-        dialogueLines = new List<string>(allLines);
-
-        if (dialogueLines.Count > 0)
-        {
-            StartCoroutine(DialogueLoop()); 
-        }
+        return new List<string>(allLines);
     }
 
-    IEnumerator DialogueLoop()
+    IEnumerator DialogueLoop(TextAsset dialogue, System.Action onFinishedCallback)
     {
+        List<string> dialogueLines = ParseDialogue(dialogue);
         textCanvas.gameObject.SetActive(true);
+        currentLine = 0;
 
         while (currentLine < dialogueLines.Count)
         {
@@ -86,5 +104,37 @@ public class NPC : MonoBehaviour
         }
 
         textCanvas.gameObject.SetActive(false);
+
+        onFinishedCallback();
+    }
+
+    public void GiveVerdict(bool accepted)
+    {
+        // hope and pray that they don't press it before they should
+        StopAllCoroutines();
+        if (accepted)
+        {
+            StartCoroutine(DialogueLoop(acceptDialogue, LeaveAccepted));
+        }
+        else
+        {
+            StartCoroutine(DialogueLoop(rejectDialogue, LeaveRejected));
+        }
+    }
+
+    void LeaveAccepted()
+    {
+        StartCoroutine(MoveTowardsTarget(endLocation));
+    }
+
+    void LeaveRejected()
+    {
+        StartCoroutine(MoveTowardsTarget(spawnLocation));
+    }
+
+    void WaitForVerdict()
+    {
+        // don't do anything :) i love having good programming practices
+        // perhaps start a coroutine for a time limit or smth. out of scope though
     }
 }
